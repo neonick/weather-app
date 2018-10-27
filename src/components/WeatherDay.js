8fd6c66304477c8d77dc2587b1895b167bb58d18
neonick <n30n1ck@gmail.com>
2018-10-28 01:47:05 +0300

    Add styled-components and my dummy components

--- src/components/WeatherDay.js
+++ src/components/WeatherDay.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react'
 import styled from "styled-components";
 
-const WeatherDayStyled = styled.div`
+const WeatherDayStyled = styled.li`
   width: 150px;
   height: 130px;
   background: ${props => (props.primary ? "lightyellow" : "white")};
+  box-shadow: ${props => (props.primary ? "0 0 3px yellow" : "none")};
   border: 1px solid #999;
-  border-radius: 10px;
-  margin-right: 30px;
+  border-radius: 7px;
+  margin-right: 20px;
   display: inline-block;
+  padding-top: 45px;
+  box-sizing: border-box;
+  position: relative;
+`;
+
+const WeekDayName = styled.span`
+  color: red;
+  font-size: 20px;
+  position: absolute;
+  top: 10px;
+  left: 10px;
+`
+
+const WeekDayStatus = styled.span`
+  color: grey;
+  text-align: right;
+  font-size: 16px;
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+`;
+
+const WeekDayDM = styled.span`
+  color: lightgrey;
+  text-align: right;
+  font-size: 20px;
+  position: absolute;
+  top: 10px;
+  right: 10px;
 `;
 
 export default class WeatherDay extends Component {
+  
 
   render() {
-    return (
-      <WeatherDayStyled 
-        primary={this.props.primary} 
-      >
-        23°C
-      </WeatherDayStyled>
-    )
+    return <WeatherDayStyled primary={this.props.primary}>
+        <WeekDayName>{this.props.dayname}</WeekDayName>
+        <WeekDayDM>{this.props.dm}</WeekDayDM>
+        днём {this.props.day || 0}
+        °C
+        <br />
+        ночью {this.props.night || 0}
+        °C
+        <WeekDayStatus>{this.props.status}</WeekDayStatus>
+      </WeatherDayStyled>;
   }
 }
--- src/components/WeatherWeek.js
+++ src/components/WeatherWeek.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import WeatherDay from "./WeatherDay";
 import styled from "styled-components";
 
-const WeatherWeekStyled = styled.div`
+const WeatherWeekStyled = styled.ul`
   margin: 200px auto;
   text-align: center;
 `
@@ -10,12 +10,13 @@ const WeatherWeekStyled = styled.div`
 export class WeatherWeek extends Component {
   render() {
     return <WeatherWeekStyled>
-        <WeatherDay primary />
-        <WeatherDay />
-        <WeatherDay />
-        <WeatherDay />
-        <WeatherDay />
-        <WeatherDay />
+        <WeatherDay dm="29.10" day="23" night="15" dayname="ПН" status="ясно" primary />
+        <WeatherDay dm="30.10" day="23" night="15" dayname="ВТ" status="ясно" />
+        <WeatherDay dm="31.10" day="23" night="15" dayname="СР" status="ясно" />
+        <WeatherDay dm="1.11" day="23" night="15" dayname="ЧТ" status="облачно" />
+        <WeatherDay dm="2.11" day="23" night="15" dayname="ПТ" status="облачно" />
+        <WeatherDay dm="3.11" day="23" night="15" dayname="СБ" status="дождь" />
+        <WeatherDay dm="4.11" day="23" night="15" dayname="ВС" status="ясно" />
       </WeatherWeekStyled>;
   }
 }
